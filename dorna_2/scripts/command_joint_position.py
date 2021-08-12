dbae26069c45f9f776dca7f46a7d35ebe7b64252
ShaySackett <11085793+ShaySackett@users.noreply.github.com>
2021-08-12 15:35:26 -0700

    Physical Robot Follows Unique Moveit Waypoints
    -Robot now successfully "follows" each unique waypoint sent to the robot arm via Rviz/Moveit over the /joint_states topic
    -Robot motion is jerky since the robot is not going from point-to-point in a continuous fashion. Right now the robot goes from point-to-point, stoppping in between each one.

--- dorna_2/scripts/command_joint_position.py
+++ dorna_2/scripts/command_joint_position.py
@@ -2,16 +2,21 @@
 import rospy
 from sensor_msgs.msg import JointState
 from dorna2 import dorna 
+import math
 
 j_0 = JointState()
-new_j_0 = float()
+j_0_JSON = float()
+old_j_0 = float()
 
 def callback_receive_JointStates(msg):
 
 	global j_0
-	global new_j_0
+	global j_0_JSON
 	j_0 = msg.position[0]
-	new_j_0 = float(j_0)
+	j_0_JSON = math.degrees(float(j_0)) #conver ROS radians to degrees for Dorna 2 API
+
+
+
 
 
 if __name__ == '__main__':
@@ -28,20 +33,23 @@ if __name__ == '__main__':
 	sub = rospy.Subscriber("/joint_states", JointState, callback_receive_JointStates) 
 	
 
-	robot.play(cmd = "jmove", rel = 0, j0 = new_j_0, vel = 10, accel = 100,jerk = 1000, id = 1)
-	robot.wait(id = 1, stat = 2)
-
-
 
 	while not rospy.is_shutdown():
-		pass
-    
+		
+
+		if old_j_0 != j_0_JSON:
+			rospy.loginfo(str(j_0_JSON))
+			robot.play(cmd = "jmove", rel = 0, j0 = j_0_JSON, vel = 10, accel = 100,jerk = 1000, id = 1)
+			#robot.wait(id = 1, stat = 2)
+			old_j_0 = j_0_JSON
+
+	    
     #close robot connection and websocket connection if robot is no longer connected
 	else:
 		robot.close()                              
 		rospy.loginfo("Robot and websocket connection ended")
 
-	rospy.spin()
+	#rospy.spin()
 
 	
 	
