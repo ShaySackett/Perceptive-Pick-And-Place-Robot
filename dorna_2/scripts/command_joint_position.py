2f12c84dbc304d211e25cd3c4311129c3054dff1
ShaySackett <11085793+ShaySackett@users.noreply.github.com>
2021-08-16 19:58:05 -0700

    Began to Add Other Joints
    -However realized that the Dorna API and the way I defined my URDF have different coordinate systems. I think I will fix this by creating a new URDF with the Dorna robot in the "all zero" configuration in line with the spec sheets on Dorna's website

--- dorna_2/scripts/command_joint_position.py
+++ dorna_2/scripts/command_joint_position.py
@@ -4,16 +4,29 @@ from sensor_msgs.msg import JointState
 from dorna2 import dorna 
 import math
 
-j_0 = JointState()
+#initialize array that stores angular position of robotic arm using JointState data type. 
+ros_position = JointState()
+old_position = JointState()
+
+#initialize variables that will store angular values of robot joints as regular floats then command robot within the Dorna API
+#Added a 'JSON' suffix to differentiate this variable from "j0,j1" etc keys in robot.play command
 j_0_JSON = float()
-old_j_0 = float()
+j_1_JSON = float()
 
 def callback_receive_JointStates(msg):
 
-	global j_0
+	#bring global variables into local scope
+	global ros_position
 	global j_0_JSON
-	j_0 = msg.position[0]
-	j_0_JSON = math.degrees(float(j_0)) #conver ROS radians to degrees for Dorna 2 API
+	global j_1_JSON
+	
+	#update position of robot arm from JointState message type
+	ros_position = msg.position
+	
+
+	#pull each joints' respective angular position from ros_position array, then convert JointState type to a float, then convert from radians in ROS to degrees for Dorna 2 API,
+	j_0_JSON = math.degrees(float(ros_position[0]))
+	j_1_JSON = (math.degrees(float(-1*ros_position[1])))  #need to add pi here since ROS and Dorna Coordinate "zeroes" are offset from one another
 
 
 
@@ -33,15 +46,18 @@ if __name__ == '__main__':
 	sub = rospy.Subscriber("/joint_states", JointState, callback_receive_JointStates) 
 	
 
+	#create a variable that causes first point to be skipped
+	first_point = True
 
 	while not rospy.is_shutdown():
 		
 
-		if old_j_0 != j_0_JSON:
-			rospy.loginfo(str(j_0_JSON))
-			robot.play(cmd = "jmove", rel = 0, j0 = j_0_JSON, vel = 10, accel = 100,jerk = 1000, id = 1)
+		if (old_position != ros_position) and first_point == False:
+			rospy.loginfo(str(j_0_JSON) + " , " +str(j_1_JSON))
+			robot.play(cmd = "jmove", rel = 0, j0 = j_0_JSON, j1 = j_1_JSON, vel = 10, accel = 100,jerk = 1000, id = 1)
 			#robot.wait(id = 1, stat = 2)
-			old_j_0 = j_0_JSON
+			old_position = ros_position
+			first_point = False
 
 	    
     #close robot connection and websocket connection if robot is no longer connected
